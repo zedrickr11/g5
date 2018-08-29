2115fc283b7b716032097a2966d6fa0818f8e273
Zedrick Rodriguez <inedc4zedrickr@gmail.com>
2018-08-29 09:10:38 -0600

    prueba en la mac

--- config/database.php
+++ config/database.php
@@ -42,10 +42,10 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', '8889'),
             'database' => env('DB_DATABASE', 'dbmantenmiento'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'root'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
