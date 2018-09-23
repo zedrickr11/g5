19f787b2d54ffe6f1e9bcf08174a16f537529ab2
ronaldpx <rconio.q@gmail.com>
2018-09-23 15:22:25 -0600

    rutas web

--- routes/web.php
+++ routes/web.php
@@ -69,6 +69,7 @@ Route::resource('equipo/rutina/tiporu','tiporuController');
 Route::resource('equipo/rutina/caracru','caracruController');
 
 Route::resource('precaucion/ejecutante','PrecaucionEjecutanteController');
+
 Route::resource('precaucion/responsable','PrecaucionResponsableController');
 
 Route::resource('peligro/naturaleza','NaturalezaPeligroController');
