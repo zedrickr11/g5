4208e78ec68a89924400715272ef4f02feed2270
ronaldpx <rconio.q@gmail.com>
2018-09-23 16:04:42 -0600

    espacios

--- routes/web.php
+++ routes/web.php
@@ -68,6 +68,8 @@ Route::resource('equipo/rutina/pruru','pruruController');
 Route::resource('equipo/rutina/tiporu','tiporuController');
 Route::resource('equipo/rutina/caracru','caracruController');
 
+
+
 Route::resource('precaucion/ejecutante','PrecaucionEjecutanteController');
 Route::resource('precaucion/responsable','PrecaucionResponsableController');
 
