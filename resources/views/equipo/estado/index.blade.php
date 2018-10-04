71b6613537e6ba0ed198953a1c94761342390c76
josebonilla16 <Josephman1253@hotmail.com>
2018-10-04 10:08:40 -0600

    actualice iconos

--- resources/views/equipo/estado/index.blade.php
+++ resources/views/equipo/estado/index.blade.php
@@ -17,8 +17,8 @@
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
           <div class="box">
             <div class="box-header">
-			  <h3 class="box-title">Listado de Estados <a href="estado/create"><button class="btn btn-success">Nuevo</button></a>
-			  		<a href="#" target="_blank"><button class="btn btn-info">Reporte</button></a></h3>
+			  <h3 class="box-title">Listado de Estados <a href="estado/create"><button class="btn btn-success"><span class="glyphicon glyphicon-plus"></span> </button></a>
+			  		<a href="#" target="_blank"><button class="btn btn-info"><span class="glyphicon glyphicon-print"></span> </button></a></h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
