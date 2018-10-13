6587405f7a6600cdd6b628d7818ccb06e8e0b08c
ramsol <ronyramsol@gmail.com>
2018-10-13 16:42:47 -0600

    actu

--- app/Http/Controllers/rumanController.php
+++ app/Http/Controllers/rumanController.php
@@ -19,6 +19,7 @@ use Response;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Input;
+
 class rumanController extends Controller
 {
   /**
