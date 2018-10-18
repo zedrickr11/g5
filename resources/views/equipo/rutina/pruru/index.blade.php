1a22ce9c0e7ffdf84985643f92f2bf594c9e1c9a
Zedrick Rodriguez <inedc4zedrickr@gmail.com>
2018-10-17 21:18:20 -0600

    andy burro jajaja

--- resources/views/equipo/rutina/pruru/index.blade.php
+++ resources/views/equipo/rutina/pruru/index.blade.php
@@ -19,7 +19,7 @@
           <div class="box">
             <div class="box-header">
 
-			  <h3 class="box-title">Listado de prueba rutina<a href="{{route('pruru/create')}}"><button class="btn btn-success"><span class="glyphicon glyphicon-plus"></span> </button></a>
+			  <h3 class="box-title">Listado de prueba rutina<a href="pruru/create"><button class="btn btn-success"><span class="glyphicon glyphicon-plus"></span> </button></a>
 			  		<a href="#" target="_blank"><button class="btn btn-info"><span class="glyphicon glyphicon-print"></span> </button></a></h3>
             </div>
             <!-- /.box-header -->
